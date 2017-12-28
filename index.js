1e8d0ef7c479b9ee0453da958e40b0d28f592a93
chenxiaochun <sjzchenxiaochun@126.com>
2017-12-28 15:03:02 +0800

    完成基本的截图功能

--- index.js
+++ index.js
@@ -11,11 +11,44 @@ function program(argv){
     commander
         .version(require('./package.json').version)
         .usage('<url> [filename] [options]')
-        .option('-v --view', 'screenshot view area')
-        .option('-f --full', 'screenshots of the entire page')
+        .option('-W --width <width>', 'set viewport width')
+        .option('-H --height <height>', 'set viewport height')
+        .option('-V --view', 'screenshot viewport')
+        .option('-F --full', 'screenshots of the full page')
+        .action((url, filename, options) => {
+            if(filename.toString() == '[object Object]'){
+                options = filename
+                filename = 'screenshort.png'
+            }
+            if(isHttpUrl(url)){
+                captor(url, filename, options)
+            }
+        })
 
     commander.parse(argv)
     if(argv.length <= 2){
         commander.help()
+    }
+}
+
+function isHttpUrl(url){
+    return url.includes('://')
+}
+
+async function captor(url, filename, options){
+    try{
+        const browser = await puppeteer.launch({
+            headless: false
+        })
+        const page = await browser.newPage()
+        await page.goto(url)
+        await page.screenshot({
+            path: filename
+        })
+        page.close()
+        browser.close()
+    }
+    catch(e){
+        console.log(e)
     }
 }

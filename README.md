6120c72db2010ff19d264672096f87f7ff7608ee
chenxiaochun <sjzchenxiaochun@126.com>
2018-01-04 18:26:45 +0800

    实现可截取具有懒加载的页面功能

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # captor
 
-页面截图助手
+一条命令生成页面截图
+
+```shell
+  Usage: captor <url> [filename] [options]
+
+
+  Options:
+
+    -V, --version          output the version number
+    -W, --width <width>    set viewport width
+    -H, --height <height>  set viewport height
+    -V, --view             screenshot viewport
+    -F, --full             screenshots of the full page
+    -h, --help             output usage information
+```
--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function program(argv){
     commander
         .version(require('./package.json').version)
         .usage('<url> [filename] [options]')
-        .option('-W --width <width>', 'set viewport width')
-        .option('-H --height <height>', 'set viewport height')
-        .option('-V --view', 'screenshot viewport')
-        .option('-F --full', 'screenshots of the full page')
+        .option('-W, --width <width>', 'set viewport width')
+        .option('-H, --height <height>', 'set viewport height')
+        .option('-V, --view', 'screenshot viewport')
+        .option('-F, --full', 'screenshots of the full page')
         .action((url, filename, options) => {
             if(filename.toString() == '[object Object]'){
                 options = filename
@@ -35,6 +35,27 @@ function isHttpUrl(url){
     return url.includes('://')
 }
 
+async function scroll(page){
+    await page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            var totalHeight = 0
+            var distance = 400
+            var timer = setInterval(() => {
+                window.scrollBy(0, distance)
+                totalHeight += distance
+    
+                if(totalHeight >= document.body.scrollHeight){
+                    clearInterval(timer)
+                    resolve()
+                }
+            }, 200)
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    })
+}
+
 async function captor(url, filename, options){
     try{
         const browser = await puppeteer.launch({
@@ -42,11 +63,23 @@ async function captor(url, filename, options){
         })
         const page = await browser.newPage()
         await page.goto(url)
-        await page.screenshot({
-            path: filename
+        await page.setViewport({
+            width: 1200,
+            height: 800
         })
-        page.close()
-        browser.close()
+
+        if(options.full){
+            await scroll(page);
+        }
+
+        console.log('📸  正在生成截图...')
+        await page.screenshot({
+            path: filename,
+            fullPage: !!options.full
+        })
+        console.log('📷  截图生成完毕')
+        await page.close()
+        await browser.close()
     }
     catch(e){
         console.log(e)
